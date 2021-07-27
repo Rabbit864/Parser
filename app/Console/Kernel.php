3031d23b71d49f9f6be7b8767c0da299c8c5e0d3
Daniil <smyshlyaevar@gmail.com>
2021-07-28 00:52:37 +0300

    feat: add cron for command parse

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command('parse:listUniversities')->everyTwoHours();
+        $schedule->command('parse:infoUniversities')->everyTwoHours();
     }
 
     /**
